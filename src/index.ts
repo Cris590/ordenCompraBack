aa6499ea83ffa3cceff45a64bd617dfd28a4c9f8
Cristian Ricardo Aragon Rodriguez <cristian.aragon@pysltda.com>
2024-09-15 12:13:36 -0500

    FIX Modificacion del cors

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,12 @@ const corsOptions = {
 
 dotenv.config();
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://ordenesdev.brtsistema.com.co');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 // view engine setup
 app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({limit: '5mb', extended: false }));
